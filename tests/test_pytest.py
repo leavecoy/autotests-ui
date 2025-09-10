1a152a3f7ecafd82432a15ce3ebe6e0c023b496b
leavecoy <pandadeluxe@mail.ru>
2025-09-10 15:12:29 +0300

    Промежуточные задания

---
+++ tests/test_pytest.py
@@ -0,0 +1,16 @@
+def test_user_login():
+    print("Login")
+
+class TestUserLogin:
+    def test_1(self):
+        pass
+
+    def test_2(self):
+        pass
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5
