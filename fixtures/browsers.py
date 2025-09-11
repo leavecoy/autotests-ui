e30e00ae42143b6a53f3f70a88296f3972c0cd20
leavecoy <pandadeluxe@mail.ru>
2025-09-11 14:50:11 +0300

    Плагины Pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -7,6 +7,7 @@ def chromium_page(playwright: Playwright) -> Page:
     yield browser.new_page()
     browser.close()
 
+
 @pytest.fixture(scope="session")
 def initialize_browser_state(playwright : Playwright):
     browser = playwright.chromium.launch(headless=False)
