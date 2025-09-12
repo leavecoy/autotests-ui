c06acfc1268418c72f06aebabfb6c088a789da5e
leavecoy <pandadeluxe@mail.ru>
2025-09-12 13:38:01 +0300

    Знакомство с PageObject

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,12 +1,11 @@
-import pytest
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import Page
 
 class BasePage:
-    def __init__(self, page):
+    def __init__(self, page: Page):
         self.page = page
 
     def visit(self, url: str):
         self.page.goto(url, wait_until='networkidle')
 
     def reload(self):
-        self.page.reload(wait_until='networkidle')
+        self.page.reload(wait_until='domcontentloaded')
