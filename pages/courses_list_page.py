f3134a2584fc9099a77142f9b9f2b3cf43059ad1
leavecoy <pandadeluxe@mail.ru>
2025-09-15 17:10:26 +0300

    Промежуточные задания

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,3 +1,4 @@
+from compontents.views.empty_view_component import EmptyViewComponent
 from pages.base_page import BasePage
 from playwright.sync_api import Page, expect
 from compontents.navigation.navbar_component import NavbarComponent
@@ -9,13 +10,10 @@ class CoursesListPage(BasePage):
 
         self.navbar = NavbarComponent(page)
         self.sidebar = SidebarComponent(page)
+        self.empty_view = EmptyViewComponent(page, identifier='courses-list')
 
         self.courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
         self.create_course_button = page.get_by_test_id('courses-list-toolbar-create-course-button')
-
-        self.empty_view_icon = page.get_by_test_id('courses-list-empty-view-icon')
-        self.empty_view_title = page.get_by_test_id('courses-list-empty-view-title-text')
-        self.empty_view_description = page.get_by_test_id('courses-list-empty-view-description-text')
 
         self.course_menu_button = page.get_by_test_id('course-view-menu-button')
         self.course_edit_menu_button = page.get_by_test_id('course-view-edit-menu-item-text')
@@ -32,15 +30,11 @@ class CoursesListPage(BasePage):
         expect(self.courses_title).to_have_text('Courses')
 
     def check_visible_empty_view(self):
-        expect(self.empty_view_icon).to_be_visible()
-
-        expect(self.empty_view_title).to_be_visible()
-        expect(self.empty_view_title).to_have_text('There is no results')
-
-        expect(self.empty_view_description).to_be_visible()
-        expect(self.empty_view_description).to_have_text(
-            'Results from the load test pipeline will be displayed here'
+        self.empty_view.check_visible(
+            title='There is no results',
+            description='Results from the load test pipeline will be displayed here'
         )
+
 
     def check_visible_create_course_button(self):
         expect(self.create_course_button).to_be_visible()
