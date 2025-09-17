19b0dbb9734ba3c461f226bd757e506b2ee2e91a
leavecoy <pandadeluxe@mail.ru>
2025-09-17 10:47:56 +0300

    pagefactory

--- compontents/charts/chart_view_component.py
+++ compontents/charts/chart_view_component.py
@@ -1,15 +1,17 @@
 from compontents.base_component import BaseComponent
-from playwright.sync_api import Page, expect
+from playwright.sync_api import Page
+from elements.text import Text
+from elements.image import Image
 
 class ChartViewComponent(BaseComponent,):
     def __init__(self, page: Page, identifier: str, chart_type: str):
         super().__init__(page)
 
-        self.title_text = page.get_by_test_id(f'{identifier}-widget-title-text')
-        self.chart = page.get_by_test_id(f'{identifier}-{chart_type}-chart')
+        self.title_text = Text(page, f'{identifier}-widget-title-text', 'Title')
+        self.chart = Image(page, f'{identifier}-{chart_type}-chart', 'Chart')
 
     def check_visible(self, title: str):
-        expect(self.title_text).to_be_visible()
-        expect(self.title_text).to_have_text(title)
+        self.title_text.check_visible()
+        self.title_text.check_have_text(title)
 
-        expect(self.chart).to_be_visible()
+        self.chart.check_visible()
