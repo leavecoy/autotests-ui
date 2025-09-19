88fad803a32b20a7063dead20ce076584c15c888
leavecoy <pandadeluxe@mail.ru>
2025-09-19 15:42:59 +0300

    Настройки автотестов с Pydantic

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,11 +1,13 @@
+from typing import Any, Generator
 import pytest
 from playwright.sync_api import Page, Playwright
 from pages.authentication.registration_page import RegistrationPage
 from _pytest.fixtures import SubRequest
 from tools.playwright.pages import initialize_playwright_page
+from config import settings
 
 @pytest.fixture
-def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
+def chromium_page(request: SubRequest, playwright: Playwright) -> Generator[Page, Any, None]:
     yield from initialize_playwright_page(playwright, test_name=request.node.name)
 
 @pytest.fixture(scope="session")
@@ -16,17 +18,22 @@ def initialize_browser_state(playwright : Playwright):
 
     registration_page = RegistrationPage(page=page)
     registration_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
-    registration_page.form.fill(email='user.name@gmail.com', username='username', password='password')
+    registration_page.form.fill(
+        email=settings.test_user.email,
+        username=settings.test_user.username,
+        password=settings.test_user.password
+    )
     registration_page.click_registration_button()
 
     # Сохраняем состояние браузера
-    context.storage_state(path='browser-state.json')
+    context.storage_state(path=settings.browser_state_file)
     browser.close()
 
 @pytest.fixture(scope="function")
-def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright : Playwright) -> Page:
+def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright : Playwright) -> Generator[
+    Page, Any, None]:
     yield from initialize_playwright_page(
         playwright,
         test_name=request.node.name,
-        storage_state='browser-state.json'
+        storage_state=settings.browser_state_file
     )
