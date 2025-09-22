23c1af4e1f2c96e3cea162249f435b084d6065cc
leavecoy <pandadeluxe@mail.ru>
2025-09-22 12:08:57 +0300

    Моки и оптимизация  автотестов

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -2,6 +2,8 @@ from typing import Any, Generator
 from playwright.sync_api import Playwright, Page
 import allure
 from config import settings, Browser
+from tools.playwright.mocks import mock_static_resources
+
 
 def initialize_playwright_page(
         playwright: Playwright,
@@ -17,6 +19,7 @@ def initialize_playwright_page(
     )
     context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
+    mock_static_resources(page)
 
     yield page
 
