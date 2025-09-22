a999657454f949cbc56279ca0dee8ea1d716cb21
leavecoy <pandadeluxe@mail.ru>
2025-09-22 14:07:07 +0300

    Добавляем информацию о переменных окружения в llure отчет. Практика работы с переменными окружения в llure

--- fixtures/allure.py
+++ fixtures/allure.py
@@ -1,7 +1,7 @@
 import pytest
-from tools.allure.environment import create_allure_environment
+from tools.allure.environment import create_allure_environment_file
 
 @pytest.fixture(scope="session", autouse=True)
 def save_environment_file():
     yield
-    create_allure_environment()
+    create_allure_environment_file()
--- tools/allure/environment.py
+++ tools/allure/environment.py
@@ -1,7 +1,14 @@
 from config import settings
+import platform
+import sys
 
-def create_allure_environment():
+
+def create_allure_environment_file():
     items = [f'{key}={value}' for key, value in settings.model_dump().items()]
+    items.extend([
+        f'os_info={platform.system()}, {platform.release()}',
+        f'python_version={sys.version}'
+    ])
     properties = '\n'.join(items)
 
     with open(settings.allure_results_dir.joinpath('environment.properties'), 'w+') as file:
