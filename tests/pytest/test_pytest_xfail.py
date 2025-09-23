26d0bfe8cb60bfa254510441c465ad820c21a23f
leavecoy <leavecoy@users.noreply.github.com>
2025-09-23 07:44:06 +0000

    deploy: f2761ca02d0578652d14196af0ece905ba72f1a7

--- tests/pytest/test_pytest_xfail.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-@pytest.mark.xfail(reason="Найден баг")
-def test_with_bug():
-    assert 1 == 2
-
-@pytest.mark.xfail(reason="Был баг")
-def test_without_bug():
-    pass
--- tools/allure/epics.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class AllureEpic(str, Enum):
-    LMS = "LMS System"
-    STUDENT = "Student system"
-    ADMINISTRATION = "Administration system"
